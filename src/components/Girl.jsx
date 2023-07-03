29f349ced7bf9a4474291cd2576a2b09a69ab2f5
yenxuan1381 <emilylo1381@gmail.com>
2023-07-03 22:43:27 +0800

    init

---
+++ src/components/Girl.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export function Girl() {
+    return (
+        <Spline scene="https://prod.spline.design/TTafelJhMXbKC0vv/scene.splinecode" />
+    );
+}
