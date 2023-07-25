ecdafa24fe8e4e8223052db2c85a67f1a1e55365
yenxuan1381 <emilylo1381@gmail.com>
2023-07-26 00:23:59 +0800

    raw

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_6u7dnbt",
+        "template_bslauog",
         {
           from_name: form.name,
           to_name: "Emily Lo",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "emilylo1381@gmail.com",
           message: `${form.message} by ${form.email}`,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "iVzYAnbw8VPiiUz9p"
       )
       .then(
         () => {
