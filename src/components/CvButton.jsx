e957749c5b67690069dadbdba183c16aa76d1e81
yenxuan1381 <emilylo1381@gmail.com>
2023-07-25 22:05:44 +0800

    added cv

--- src/components/CvButton.jsx
+++ src/components/CvButton.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 const CvButton = () => {
     return ( 
         <button class="button lg:scale-100  xs:scale-80 ">
-            <a href="/src/assets/1.png" download>
+            <a href="/src/assets/EmilyLo_CV.pdf" download>
                 <span class="button-content">Download CV</span>
             </a>
         </button>
