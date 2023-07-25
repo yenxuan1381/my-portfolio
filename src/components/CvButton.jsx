c293796314b5826f09ed60606a3ffc0ceb7a19cf
yenxuan1381 <emilylo1381@gmail.com>
2023-07-26 00:03:03 +0800

    added target="_blank" for CV download

--- src/components/CvButton.jsx
+++ src/components/CvButton.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 const CvButton = () => {
     return ( 
         <button class="button lg:scale-100  xs:scale-80 ">
-            <a href="https://drive.google.com/file/d/1gefGVZ4nFkE4N1yAZurMvoh9xh2SPCFK/view?usp=sharing" download>
+            <a href="https://drive.google.com/file/d/1gefGVZ4nFkE4N1yAZurMvoh9xh2SPCFK/view?usp=sharing" download target="_blank">
                 <span class="button-content">Download CV</span>
             </a>
         </button>
