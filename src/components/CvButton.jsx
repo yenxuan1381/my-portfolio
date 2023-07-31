8fafbafac7ac39c4fa34b9812b648c5c8288ee5e
Emily <76611914+yenxuan1381@users.noreply.github.com>
2023-07-31 22:53:49 +0800

    Update CvButton.jsx

--- src/components/CvButton.jsx
+++ src/components/CvButton.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 const CvButton = () => {
     return ( 
         <button class="button lg:scale-100  xs:scale-80 ">
-            <a href="https://drive.google.com/file/d/1gefGVZ4nFkE4N1yAZurMvoh9xh2SPCFK/view?usp=sharing" download target="_blank">
+            <a href="https://drive.google.com/file/d/1zWKWJUb9J3j-VKIpzqe60faQuOUHlV7r/view?usp=sharing" download target="_blank">
                 <span class="button-content">Download CV</span>
             </a>
         </button>
